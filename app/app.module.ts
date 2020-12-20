2af5d04f4c7c789f7d5b3760f0de50cac41c695a
Orest Horak <31936833+Orest-Horak@users.noreply.github.com>
2020-12-21 00:37:13 +0200

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatNativeDateModule } from '@angular/material';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemoMaterialModule } from './demo-material.module';
 import { AppRoutingModule } from './app-routing.module';
-import { DemoRoutingModule } from './demo/demo-routing.module';
 import { TestRoutingModule } from './test/test-routing.module';
 import { TestModule } from './test/test.module';
 import { AppComponent } from './app.component';
@@ -16,8 +14,16 @@ import { HelloComponent } from './hello.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { TestComponent } from './test/test.component';
+import { ClarityModule } from 'clarity-angular';
+import { LoginComponent } from './login/login.component';
+import { UserService } from './user.service';
+
 @NgModule({
   imports: [
+    BrowserModule, 
+    FormsModule, 
+    ClarityModule,
+    HttpClientModule, 
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
@@ -35,10 +41,11 @@ import { TestComponent } from './test/test.component';
     AppComponent,
     WelcomeComponent,
     HelloComponent, 
-    SignUpComponent, 
+    LoginComponent,
+    SignUpComponent,
     TestComponent]
   ,
   bootstrap: [AppComponent],
-  providers: []
+  providers: [UserService]
 })
 export class AppModule { }
