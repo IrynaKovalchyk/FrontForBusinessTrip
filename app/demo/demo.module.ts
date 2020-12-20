e5219276d6b613a85aed62100ba6879795ae6dcc
IrynaKovalchyk <irenkovalchik@gmail.com>
2020-12-20 20:38:32 +0200

    1 file modified

--- app/demo/demo.module.ts
+++ app/demo/demo.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonDemoComponent } from './button-demo/button-demo.component';
-
+ 
 @NgModule({
   declarations: [
     ButtonDemoComponent
