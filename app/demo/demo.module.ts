897cd9347fff89e4eca8bf0227ed753bf44ede57
Orest Horak <31936833+Orest-Horak@users.noreply.github.com>
2020-12-20 19:04:17 +0200

    Update demo.module.ts
    error fixed

--- app/demo/demo.module.ts
+++ app/demo/demo.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 import { ButtonDemoComponent } from './button-demo/button-demo.component';
-import { InputDemoComponent } from './input-demo/input-demo.component';
-import { SnackbarDemoComponent } from './snackbar-demo/snackbar-demo.component';
-
 
 @NgModule({
   declarations: [
-    ButtonDemoComponent,
-    InputDemoComponent,
-    SnackbarDemoComponent
+    ButtonDemoComponent
   ],
   imports: [
     CommonModule,
